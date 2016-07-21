780d49844d521d9e78cb3001f843fcbed48245ec
Jamie <up733026@myport.ac.uk>
2016-07-21 11:59:35 +0100

    Trello sorted

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160721093117) do
-=======
 ActiveRecord::Schema.define(version: 20160721101712) do
->>>>>>> 9d8b5b99f3c259d94d231a294296984689a5fbff
 
   create_table "bugs", force: :cascade do |t|
     t.string   "title"
